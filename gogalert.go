d6c0f49458c617fe056eef489cd32087943eecae
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-07-24 11:34:06 +0300

    Simplified cli; Added options: --list-(nodes|clusters|metrics)

--- gogalert.go
+++ gogalert.go
@@ -3,22 +3,25 @@ package main
 import (
 	"./ganglia/api"
 	"./ganglia/response"
-	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	node        = kingpin.Flag("node", "Node name to search").String()
-	cluster     = kingpin.Flag("cluster", "Cluster name to search").String()
-	metric      = kingpin.Flag("metric", "Metric name to search").String()
-	threshhold  = kingpin.Flag("threshhold", "Threshhold value").Int()
-	condition   = kingpin.Flag("condition", "Condition").Enum("eq", "gt", "ge", "lt", "le")
-	listMetric  = kingpin.Flag("list-metrics", "List metrics").Bool()
-	listNodes   = kingpin.Flag("list-nodes", "List nodes").Bool()
-	lisClusters = kingpin.Flag("list-clusters", "List clusters").Bool()
-	localPath   = kingpin.Flag("file", "Read gmeta response from local file").ExistingFile()
-	remoteIP    = kingpin.Flag("host", "Read gmeta response from host, default: 127.0.0.1").Default("127.0.0.1").String()
-	remotePort  = kingpin.Flag("port", "Read gmeta response from port, default: 8651").Default("8651").Int()
+	// filters
+	node    = kingpin.Flag("node", "Node name to search").String()
+	cluster = kingpin.Flag("cluster", "Cluster name to search").String()
+	metric  = kingpin.Flag("metric", "Metric name to search").String()
+	// conditions
+	threshhold = kingpin.Flag("threshhold", "Threshhold value").Int()
+	condition  = kingpin.Flag("condition", "Condition").Enum("eq", "gt", "ge", "lt", "le")
+	// source of the xml
+	localPath  = kingpin.Flag("file", "Read gmeta response from local file").ExistingFile()
+	remoteIP   = kingpin.Flag("host", "Read gmeta response from host, default: 127.0.0.1").Default("127.0.0.1").String()
+	remotePort = kingpin.Flag("port", "Read gmeta response from port, default: 8651").Default("8651").Int()
+	// list objects
+	listMetric   = kingpin.Flag("list-metrics", "List metrics").Bool()
+	listNodes    = kingpin.Flag("list-nodes", "List nodes").Bool()
+	listClusters = kingpin.Flag("list-clusters", "List clusters").Bool()
 )
 
 func main() {
@@ -34,11 +37,17 @@ func main() {
 		gResp = api.ParseSocket(*remoteIP, *remotePort)
 	}
 
+	s := api.NewGSet()
 	for _, metric := range gResp.Find(&filter) {
-		fmt.Println(metric.GCluster)
-		fmt.Println(metric.GHost)
-		fmt.Println(metric.GMetric)
-		fmt.Println()
+		if *listClusters {
+			s.PrintIfNotInSet(metric.GCluster.Name)
+		}
+		if *listNodes {
+			s.PrintIfNotInSet(metric.GHost.Name)
+		}
+		if *listMetric {
+			s.PrintIfNotInSet(metric.GMetric.Name)
+		}
 	}
 
 }
