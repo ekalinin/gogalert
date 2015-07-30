fd6145515b42e7283511735795ec6f05d1d8340e
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-07-30 14:45:31 +0300

    added deamon mode

--- gogalert.go
+++ gogalert.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"./ganglia/api"
-	"./ganglia/response"
+	"./gogalert/deamon"
+	"./gogalert/ganglia/api"
+	"./gogalert/ganglia/response"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -16,38 +17,37 @@ var (
 	condition  = kingpin.Flag("condition", "Condition").Enum("eq", "gt", "ge", "lt", "le")
 	// source of the xml
 	localPath  = kingpin.Flag("file", "Read gmeta response from local file").ExistingFile()
-	remoteIP   = kingpin.Flag("host", "Read gmeta response from host, default: 127.0.0.1").Default("127.0.0.1").String()
+	remoteHost = kingpin.Flag("host", "Read gmeta response from host, default: 127.0.0.1").Default("127.0.0.1").String()
 	remotePort = kingpin.Flag("port", "Read gmeta response from port, default: 8651").Default("8651").Int()
 	// list objects
 	listMetric   = kingpin.Flag("list-metrics", "List metrics").Bool()
 	listNodes    = kingpin.Flag("list-nodes", "List nodes").Bool()
 	listClusters = kingpin.Flag("list-clusters", "List clusters").Bool()
+	// deamon mode
+	deamonMode = kingpin.Flag("deamon", "Run in deamon mode").Bool()
 )
 
 func main() {
 
 	kingpin.Parse()
 
-	gResp := response.GMetaResponse{}
-	filter := response.MetricFilter{*metric, *node, *cluster, *condition, *threshhold}
+	filter := &response.MetricFilter{*metric, *node, *cluster, *condition, *threshhold}
+	source := &api.DataSource{*localPath, *remoteHost, *remotePort}
 
-	if *localPath != "" {
-		gResp = api.ParseFile(*localPath)
+	if *deamonMode {
+		deamon.NewStatServer(source, filter).Start()
 	} else {
-		gResp = api.ParseSocket(*remoteIP, *remotePort)
-	}
-
-	s := api.NewGSet()
-	for _, metric := range gResp.Find(&filter) {
-		if *listClusters {
-			s.PrintIfNotInSet(metric.GCluster.Name)
-		}
-		if *listNodes {
-			s.PrintIfNotInSet(metric.GHost.Name)
-		}
-		if *listMetric {
-			s.PrintIfNotInSet(metric.GMetric.Name)
+		s := api.NewGSet()
+		for _, metric := range api.Parse(source).Find(filter) {
+			if *listClusters {
+				s.PrintIfNotInSet(metric.GCluster.Name)
+			}
+			if *listNodes {
+				s.PrintIfNotInSet(metric.GHost.Name)
+			}
+			if *listMetric {
+				s.PrintIfNotInSet(metric.GMetric.Name)
+			}
 		}
 	}
-
 }
