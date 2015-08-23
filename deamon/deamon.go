b0a19a94c79c0d0e760990d1191c92a82aee55d9
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-08-23 14:50:18 +0300

    better deamon logic (added handlers for several system signals)

--- deamon/deamon.go
+++ deamon/deamon.go
@@ -3,12 +3,15 @@ package deamon
 import (
 	"fmt"
 	"github.com/ekalinin/gogalert/api"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 type StatServer struct {
 	// Period to refresh data from ganglia
-	refreshRate time.Duration
+	refreshRate int
 	// Source of the XML data
 	source *api.DataSource
 	// threshholds
@@ -20,16 +23,39 @@ type StatServer struct {
 }
 
 func NewStatServer(source *api.DataSource, threshholds *api.MetricFilter, configPath string) *StatServer {
-	return &StatServer{2 * time.Minute, source, threshholds, configPath, nil}
+	return &StatServer{60, source, threshholds, configPath, nil}
 }
 
 func (this *StatServer) Start() error {
 	fmt.Println(" Server: started, config: ", *this)
 	this.config = ParseConfig(this.configPath)
+	this.refreshRate = this.config.Interval
 	fmt.Println(" Server: parsed config: ", *this.config)
+
+	go this.CollectStats()
+
+	signalChan := make(chan os.Signal, 1)
+	// http://adampresley.com/2015/02/16/waiting-for-goroutines-to-finish-running-before-exiting.html
+	// https://gist.github.com/reiki4040/be3705f307d3cd136e85
+	signal.Notify(signalChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	needStop := false
 	for {
-		go this.CollectStats()
-		time.Sleep(this.refreshRate)
+		select {
+		case <-time.After(time.Duration(this.refreshRate) * time.Second):
+			go this.CollectStats()
+		case <-signalChan:
+			fmt.Println("Stopping...")
+			needStop = true
+		}
+
+		if needStop {
+			break
+		}
 	}
 
 	return nil
